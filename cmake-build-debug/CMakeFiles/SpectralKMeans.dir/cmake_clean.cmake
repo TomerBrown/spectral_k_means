8e4ff4eb8a371463db6ad762ff7ae2f786253261
TomerBrown <73110109+TomerBrown@users.noreply.github.com>
2021-08-10 19:24:24 +0300

    Python draft - need more testing

---
+++ cmake-build-debug/CMakeFiles/SpectralKMeans.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpectralKMeans.dir/main.c.obj"
+  "SpectralKMeans.exe"
+  "SpectralKMeans.exe.manifest"
+  "SpectralKMeans.pdb"
+  "libSpectralKMeans.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SpectralKMeans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
